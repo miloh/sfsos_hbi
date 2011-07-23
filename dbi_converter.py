9ffcab2540a8692928d7958c7fea3d38f0253730
R. Miloh Alexander <miloh@froggytoad.net>
2011-07-23 15:37:13 -0700

    updating the python script

--- dbi_converter.py
+++ dbi_converter.py
@@ -4,16 +4,30 @@
 
 from xlrd import *
 from sqlalchemy import *
+import os
+import glob
 
+# parse directory wiwth *.xls files and iterate to open workbooks, convert to sql format  (all columns to a table exicept for the messy 'description' colum)
 
-# parse directory wiwth *.xls files and iterate to open workbooks, convert to sql format  (all columns to a table except for the messy 'description' colum)
+#  OK 
+path = './'
+
+for infile in glob.glob(os.path.join(path,'*.xls')):
+ print infile
 wb = open_workbook('June2009Issued_Permits.xls')
+#wb = open_workbook('November2008issued_permits.xls')
 
-for s in  wb.sheets():
-	 print 'Sheet:',s.name
-	 for row in range(s.nrows):
-			values = []
-			for col in range(s.ncols):
-					values.append(s.cell(row,col).value)
-			print ','.join(values)
-	  print
+#  decomment for inline thiing of all *.xls in a directory
+#wb = open_workbook(inline)
+      
+#for s in  wb.sheets():
+#      	 print 'Sheet:',s.name
+#      	 for row in range(s.nrows):
+#      			values = []
+#      			for col in range(s.ncols):
+#      					values.append(s.cell(row,col).value)
+#      			print ','.join(values)
+#         print
+
+
+
