328c4671d194f41c546ac0baed9b9abcbbaac64f
Miloh <miloh@li90-197.members.linode.com>
2011-07-24 00:38:23 +0000

    the sfdbi xls files should be in a parallel directory

--- dbi_converter.py
+++ dbi_converter.py
@@ -1,33 +1,33 @@
-# python yo
-
-
+# python
 
 from xlrd import *
 from sqlalchemy import *
 import os
 import glob
 
-# parse directory wiwth *.xls files and iterate to open workbooks, convert to sql format  (all columns to a table exicept for the messy 'description' colum)
 
-#  OK 
-path = './'
+# parse directory wiwth *.xls files and iterate to open workbooks, convert to sql format  (all columns to a table except for the messy description colum)
 
+#  OK
+path = '../sfdbi/'
+
+data = {}
 for infile in glob.glob(os.path.join(path,'*.xls')):
- print infile
-wb = open_workbook('June2009Issued_Permits.xls')
-#wb = open_workbook('November2008issued_permits.xls')
+#afile = {}
+#afile['filename'] = infile
 
-#  decomment for inline thiing of all *.xls in a directory
-#wb = open_workbook(inline)
-      
-#for s in  wb.sheets():
-#      	 print 'Sheet:',s.name
-#      	 for row in range(s.nrows):
-#      			values = []
-#      			for col in range(s.ncols):
-#      					values.append(s.cell(row,col).value)
-#      			print ','.join(values)
-#         print
+# print infile
+#wb = open_workbook(November2008issued_permits.xls
 
+#  decomment for inline thing of all *.xls in a directory
+ wb = open_workbook(infile)
+
+#  wb.sheet()[0]  is the sheet object... assume there is only 1/file 
+ sheet = wb.sheets()[0]
+ colnames = []
+ testrange = [3,6]   # add in cols 17,18,19,20,21 for address data...
+ for col in testrange:     # range(sheet.ncols):
+  colnames.append(sheet.cell(0, col))
+ data[infile] = {'colnames': colnames}
 
 
